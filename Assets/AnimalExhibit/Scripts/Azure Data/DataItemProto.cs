1699c81881b4dbfd4bfa45b30f5fd273510f0f07
SCOOBY-SURFACE\andre <andrew-lyall@hotmail.com>
2019-09-22 13:19:28 +1200

    iteration 1 completed

---
+++ Assets/AnimalExhibit/Scripts/Azure Data/DataItemProto.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataItemProtoDino {
+    //public int DataItemNo { get; set; }
+
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+
+    public float rotationX { get; set; }
+    public float rotationY { get; set; }
+    public float rotationZ { get; set; }
+
+    public float Size { get; set; }
+    public string Name { get; set; }
+       
+
+}
