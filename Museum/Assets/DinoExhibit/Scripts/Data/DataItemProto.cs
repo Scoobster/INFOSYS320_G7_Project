7ac04497863633774a7358ff1f2e787c896333a2
SCOOBY-SURFACE\andre <andrew-lyall@hotmail.com>
2019-10-08 12:53:14 +1300

    Iteration 2 complete

---
+++ Museum/Assets/DinoExhibit/Scripts/Data/DataItemProto.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataItemProto
+{
+    //public int DataItemNo { get; set; }
+
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+
+    public float rotationX { get; set; }
+    public float rotationY { get; set; }
+    public float rotationZ { get; set; }
+
+    //public float Size { get; set; }
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+    public string Length { get; set; }
+    public string Weight { get; set; }
+    public string Speed { get; set; }
+    public string Lifespan { get; set; }
+
+
+}
